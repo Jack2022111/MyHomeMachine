23cc161ca4dc987020ca89a874181d1d550a5e7d
John Wimmer <jzipper@outlook.com>
2025-02-19 14:19:43 -0500

    Revert "MQTT"
    This reverts commit 6d1206bcefd9f4b3a7cae06bbe07102e734da844.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,34 +49,25 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    // MQTT
-    implementation(libs.org.eclipse.paho.client.mqttv3)
-    implementation(libs.org.eclipse.paho.android.service)
-
-    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
-    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
-
-    implementation(libs.kotlinx.coroutines.android)
-
-    implementation(libs.okhttp)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.constraintlayout)
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
 
     // For animations
-    implementation(libs.accompanist.navigation.animation)
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.30.1")
 
     // For extended icons
-    implementation(libs.androidx.material.icons.extended)
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
     // For custom fonts
-    implementation(libs.androidx.ui.text.google.fonts)
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
     // For coroutines
-    implementation(libs.kotlinx.coroutines.android)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Add Jetpack Compose Navigation
-    implementation(libs.androidx.navigation.compose)
+    implementation("androidx.navigation:navigation-compose:2.7.2")
 
-    implementation (libs.androidx.lifecycle.viewmodel.ktx)
-    implementation (libs.androidx.lifecycle.runtime.ktx.v287)
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 
     // Test dependencies
     testImplementation(libs.junit)
